8f4d422f68ae96c8ffec47b6660a749e15c32f45
Stephen Schrauger <github@schrauger.com>
2014-11-29 22:14:15 -0500

    Update Photopass_Downloader.meta.js

--- Photopass_Downloader.meta.js
+++ Photopass_Downloader.meta.js
@@ -8,6 +8,6 @@
 // @version     1.2
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js
 // @grant       none
-// @downloadURL https://raw.githubusercontent.com/schrauger/Disney-Photopass-Downloader/master/Photopass_Downloader.js
-// @updateURL   https://raw.githubusercontent.com/schrauger/Disney-Photopass-Downloader/master/Photopass_Downloader.js
+// @downloadURL https://raw.githubusercontent.com/schrauger/Disney-Photopass-Downloader/master/Photopass_Downloader.user.js
+// @updateURL   https://raw.githubusercontent.com/schrauger/Disney-Photopass-Downloader/master/Photopass_Downloader.meta.js
 // ==/UserScript==
