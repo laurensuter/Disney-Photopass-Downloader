54434a36ef3483fbe6d34aa38ce35fa85a34d557
Stephen Schrauger <github@schrauger.com>
2014-11-29 22:09:07 -0500

    Update Photopass_Downloader.js

--- Photopass_Downloader.js
+++ Photopass_Downloader.js
@@ -5,7 +5,7 @@
 // @include     https://mydisneyphotopass.disney.go.com/mymedia/
 // @include     https://mydisneyphotopass.disney.go.com/mymedia
 // @include     https://mydisneyphotopass.disney.go.com/mymedia/*
-// @version     1.1
+// @version     1.2
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js
 // @grant       none
 // @downloadURL https://raw.githubusercontent.com/schrauger/Disney-Photopass-Downloader/master/Photopass_Downloader.js
