e72376e4d28481089edd0bbe18d4c683b140ac80
Stephen Schrauger <stephen@schrauger.com>
2014-12-11 11:38:12 -0500

    Begin support for filenames and direct downloads

--- greasemonkey/Photopass_Downloader.user.js
+++ greasemonkey/Photopass_Downloader.user.js
@@ -11,13 +11,83 @@
 // @downloadURL https://raw.githubusercontent.com/schrauger/Disney-Photopass-Downloader/master/greasemonkey/Photopass_Downloader.user.js
 // @updateURL   https://raw.githubusercontent.com/schrauger/Disney-Photopass-Downloader/master/greasemonkey/Photopass_Downloader.user.js
 // ==/UserScript==
+/*jslint browser: true*/
+/*global jQuery*/
 jQuery(document).ready(function () {
-    var max_photos = +jQuery('#totalMediaCount').text(); // number of photos in your account
+    "use strict";
+    var max_photos = +jQuery('#totalMediaCount').text(); // number of photos in your account (the '+' character forces it to be a number instead of a string)
+    var medium_url_list; // contains key->value of unique_id->url
+    var photo_unique_ids; // contains 0-based count array with value of unique_id
+    jQuery.getJSON("/slideshow/index/getMediumres", function (data) {
+        // contains the urls for medium resolution, as well as the unique key
+        medium_url_list = data;
+        jQuery.each(medium_url_list, function (index, value) {
+            photo_unique_ids.push(index); // an array of just the unique ids. This way, for ranges, we don't have to loop and count multiple times; just use the index of this array.
+        });
+    });
+    var photo_detail_list; // contains raw object data with photo details
+    var photo_detail_list_array; // contains key->value of unique_id->json_object
+    jQuery.getJSON("/disney/ajax/getGuestMedia", function (data) {
+        // contains the time and other information, as well as the unique key
+        photo_detail_list = data.guestMedia;
+        jQuery.each(photo_detail_list, function (index, value) {
+            photo_detail_list_array[value.guestMediaId] = value; // array index by unique id for easy lookup later
+        });
+    });
+
+    /**
+     * A very simple date formatter. Takes the input javascript Date object and returns a string.
+     * @param date
+     */
+    function format_date(date) {
+        var str_return = '';
+        str_return += date.getFullYear();
+        str_return += '-' + date.getMonth();
+        str_return += '-' + date.getDate();
+        str_return += ' ' + date.getHours();
+        str_return += '_' + date.getMinutes();
+        str_return += '_' + date.getSeconds();
+        return str_return;
+    }
+
+    /**
+     * Downloads the photo based on the unique id as listed in both JSON requests.
+     * @param unique_id
+     */
+    function download_photo(unique_id) {
+        var url = medium_url_list[unique_id];
+        var date_created = photo_detail_list_array[unique_id].takenDate;
+        date_created = new Date(Date.parse(date_created)); // javascript can automatically parse the date given by Disney
+        var media_type = photo_detail_list_array[unique_id].mediaType;
+        var file_extension = '.jpg'; // default to jpg
+
+        var date_created_string = format_date(date_created);
+
+    }
+
+    /**
+     * Opens some or all of the available photos in new tabs to download them.
+     * @param start Start range inclusive (1-based)
+     * @param stop Stop range inclusive (if this is greater than available, it will stop at max available)
+     */
+    function download(start, stop) {
+        if (start <= max_photos) {
+            var i;
+            for (i = 0; i <= stop && i <= max_photos; i++) {
+                download_photo(photo_unique_ids[i]);
+            }
+        }
+    }
+
+
+
+
+
     jQuery("div.mosaicHeaderRightTable").after("<div style='position: relative; z-index: 100;'><a href='#' class='download_all' title='Warning - Clicking this link will open ALL of your photos in individual windows'>Open All</a> | " +
                                                    "<a href='#' class='download_range' title='Clicking this link will open a specified range of your photos in individual windows'>Open </a> " +
                                                    "<input size='2' name='photopassdownload_start' value='1' min='1' max='" + max_photos + "' type='number' style='width:3em;' step='20'/> through <label name='photopassdownload_end'>20</label></div>");
     // add links to download all or some photos
-    
+
     jQuery("a.download_all").click(function () {
         // when 'download all' is clicked, get medium res photos and open each in a new tab
         jQuery.getJSON("/slideshow/index/getMediumres", function (data) {
